2fa8db74d6f009ec83c901f8b6c80d77b73d7e0a
kristina <terekhova@cip.ifi.lmu.de>
2025-10-18 13:54:11 +0200

    Created a finished classifier using sklearn. Not yet tested

--- Classification and Clustering/simple_classifier.py
+++ Classification and Clustering/simple_classifier.py
@@ -1,9 +1,9 @@
-from pathlib import Path
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+from sklearn import metrics
 import argparse
 import json
-import pandas as pd
 import sys
 
 class Classifier:
@@ -14,24 +14,32 @@ class Classifier:
         with open(test_path) as test:
             self.testing_data = json.load(test)
 
-        # TODO: split training sentences into tokens to pass into vectorizer
-        example_sentences = [literature_dict["text"] for literature_dict in self.training_data]
-        # TODO: convert list of lists of tokens into input matrix
-        self.vectorizer = TfidfVectorizer(tokenizer=lambda text: text.split(), stop_words='german')
-        # TODO: fit vectorizer to training sentences
+        # Extract the relevant data from json file
+        self.training_sentences = [literature_dict["text"] for literature_dict in self.training_data]
+        self.training_labels = [literature_dict["author"] for literature_dict in self.training_data]
+
+        self.eval_sentences = [literature_dict["text"] for literature_dict in self.testing_data]
+        self.eval_labels = [literature_dict["author"] for literature_dict in self.testing_data]
+
+        # Create model pipeline
+        self.model = make_pipeline(TfidfVectorizer(), LogisticRegression())
 
 
     def training(self):
         # Train on training sentences using Logistic Regression
-        # TODO: Train LogisticRegression model on training sentences (how to integrate vectorizer?)
-        pass
+        self.model.fit(self.training_sentences, self.training_labels)
 
 
 
     def evaluation(self):
-        # TODO: Run the trained LogisticRegression model on evaluation sentences
-        # TODO: Evaluate performance using Precision, Recall and F1-Score (Seaborn heat map?)
-        pass
+        # Run the trained LogisticRegression model on evaluation sentences
+        predicted_labels = self.model.predict(self.eval_sentences)
+
+        # Evaluate performance using Precision, Recall and F1-Score (Seaborn heat map?)
+        precision = metrics.precision_score(self.eval_labels, predicted_labels)
+        recall = metrics.recall_score(self.eval_labels, predicted_labels)
+        f1_score = metrics.f1_score(self.eval_labels, predicted_labels)
+
 
 
 if __name__ == '__main__':
