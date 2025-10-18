0e7a1463e4e8db5099f916a86e688920ed8a7276
kristina <terekhova@cip.ifi.lmu.de>
2025-10-18 10:49:04 +0200

    Defined tasks to complete to get the classifier running

--- Classification and Clustering/simple_classifier.py
+++ Classification and Clustering/simple_classifier.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 import argparse
 import json
+import pandas as pd
 import sys
 
 class Classifier:
@@ -13,13 +14,23 @@ class Classifier:
         with open(test_path) as test:
             self.testing_data = json.load(test)
 
-    def training(self):
+        # TODO: split training sentences into tokens to pass into vectorizer
         example_sentences = [literature_dict["text"] for literature_dict in self.training_data]
-        vectorizer = TfidfVectorizer(tokenizer=lambda text: text.split(), stop_words='german')
+        # TODO: convert list of lists of tokens into input matrix
+        self.vectorizer = TfidfVectorizer(tokenizer=lambda text: text.split(), stop_words='german')
+        # TODO: fit vectorizer to training sentences
+
+
+    def training(self):
+        # Train on training sentences using Logistic Regression
+        # TODO: Train LogisticRegression model on training sentences (how to integrate vectorizer?)
+        pass
 
 
 
     def evaluation(self):
+        # TODO: Run the trained LogisticRegression model on evaluation sentences
+        # TODO: Evaluate performance using Precision, Recall and F1-Score (Seaborn heat map?)
         pass
 
 
